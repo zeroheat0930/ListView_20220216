5384e0f94eb7cad7440c976238ff0d83228a14d9
zeroheat0930 <dongjunkorea@naver.com>
2022-02-16 16:03:42 +0900

    어댑터를 실제로 만들어서 > 리스트뷰와 연결

--- app/src/main/java/com/zeroheat/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/zeroheat/listview_20220216/MainActivity.kt
@@ -2,10 +2,13 @@ package com.zeroheat.listview_20220216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zeroheat.listview_20220216.adapters.StudentAdapter
 import com.zeroheat.listview_20220216.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdapter : StudentAdapter
     val mStudentList = ArrayList<StudentData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,16 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mStudentList.add(StudentData("조경진", 1988, "서울시 동대문구"))
-        mStudentList.add(StudentData("고재천", 1988, "서울시 동대문구"))
+        mStudentList.add(StudentData("고재천", 1988, "서울시 중랑구"))
         mStudentList.add(StudentData("방우진", 1983, "경기도 고양시"))
-        mStudentList.add(StudentData("서정민", 1984, "서울시 동대문구"))
-        mStudentList.add(StudentData("이승민", 1994, "서울시 동대문구"))
-        mStudentList.add(StudentData("이영종", 1991, "서울시 동대문구"))
-        mStudentList.add(StudentData("이윤주", 1989, "서울시 동대문구"))
-        mStudentList.add(StudentData("이진실", 1994, "서울시 동대문구"))
-        mStudentList.add(StudentData("정동준", 1992, "서울시 동대문구"))
-        mStudentList.add(StudentData("최재훈", 1994, "서울시 동대문구"))
+        mStudentList.add(StudentData("서정민", 1984, "경기도 광명시"))
+        mStudentList.add(StudentData("이승민", 1994, "경기도 안양시"))
+        mStudentList.add(StudentData("이영종", 1991, "경기도 고양시"))
+        mStudentList.add(StudentData("이윤주", 1989, "서울시 강서구"))
+        mStudentList.add(StudentData("이진실", 1994, "서울시 송파구"))
+        mStudentList.add(StudentData("정동준", 1992, "경기도 남양주시"))
+        mStudentList.add(StudentData("최재훈", 1994, "경기도 수원시"))
         mStudentList.add(StudentData("이성노", 1988, "경기도 남양주시"))
+
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+        studentListView.adapter = mAdapter
 
     }
 }
