2c5fcd392605391edda8a8c98ffac211b2d17e5f
zeroheat0930 <dongjunkorea@naver.com>
2022-02-16 15:15:35 +0900

    arrayadapter 상속을 받을때, 부모한테 넘겨줄 필요한 재료들을 자녀 클래스가 받아줌

--- app/src/main/java/com/zeroheat/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/zeroheat/listview_20220216/adapters/StudentAdapter.kt
@@ -1,4 +1,12 @@
 package com.zeroheat.listview_20220216.adapters
 
-class StudentAdapter {
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.zeroheat.listview_20220216.datas.StudentData
+
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>
+) : ArrayAdapter<StudentData>(mContext,resId,mList) {
 }
