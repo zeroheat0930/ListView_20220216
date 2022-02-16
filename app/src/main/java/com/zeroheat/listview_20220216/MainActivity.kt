f76a7e98d420605848b636dd3202cfeb9487c26e
zeroheat0930 <dongjunkorea@naver.com>
2022-02-16 16:42:56 +0900

    이벤트 추가

--- app/src/main/java/com/zeroheat/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/zeroheat/listview_20220216/MainActivity.kt
@@ -37,7 +37,18 @@ class MainActivity : AppCompatActivity() {
         val clickedStudent = mStudentList[position]
 
             Toast.makeText(this, "${clickedStudent.name}학생 클릭됨", Toast.LENGTH_SHORT).show()
-            
+
+        studentListView.setOnItemLongClickListener { parent, view, position, id ->
+
+            val longClickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${longClickedStudent.name}학생이 길게 클릭됨", Toast.LENGTH_SHORT).show()
+
+            return@setOnItemLongClickListener true
+
+        }
+
+
         }
     }
 }
