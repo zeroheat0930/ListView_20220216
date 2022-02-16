23b65daf58f44dedee49f60cc0242c87ccc332b0
zeroheat0930 <dongjunkorea@naver.com>
2022-02-16 15:36:23 +0900

    tempRow를 검사해서, null이라면 - 새로 xml을 끌어와서 대입시키기.

--- app/src/main/java/com/zeroheat/listview_20220216/adapters/StudentAdapter.kt
+++ app/src/main/java/com/zeroheat/listview_20220216/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.zeroheat.listview_20220216.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.zeroheat.listview_20220216.R
 import com.zeroheat.listview_20220216.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,14 @@ class StudentAdapter(
     val resId: Int,
     val mList: ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext,resId,mList) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.student_list_item, null)
+
+        }
+
+    }
 }
