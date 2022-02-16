48d31345627e81d4f5eeb07f623b184d6fb9ce0d
zeroheat0930 <dongjunkorea@naver.com>
2022-02-16 15:55:19 +0900

    학생 데이터 생성 > 목록 등록

--- app/src/main/java/com/zeroheat/listview_20220216/MainActivity.kt
+++ app/src/main/java/com/zeroheat/listview_20220216/MainActivity.kt
@@ -2,10 +2,27 @@ package com.zeroheat.listview_20220216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zeroheat.listview_20220216.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(StudentData("조경진", 1988, "서울시 동대문구"))
+        mStudentList.add(StudentData("고재천", 1988, "서울시 동대문구"))
+        mStudentList.add(StudentData("방우진", 1983, "경기도 고양시"))
+        mStudentList.add(StudentData("서정민", 1984, "서울시 동대문구"))
+        mStudentList.add(StudentData("이승민", 1994, "서울시 동대문구"))
+        mStudentList.add(StudentData("이영종", 1991, "서울시 동대문구"))
+        mStudentList.add(StudentData("이윤주", 1989, "서울시 동대문구"))
+        mStudentList.add(StudentData("이진실", 1994, "서울시 동대문구"))
+        mStudentList.add(StudentData("정동준", 1992, "서울시 동대문구"))
+        mStudentList.add(StudentData("최재훈", 1994, "서울시 동대문구"))
+        mStudentList.add(StudentData("이성노", 1988, "경기도 남양주시"))
+
     }
 }
